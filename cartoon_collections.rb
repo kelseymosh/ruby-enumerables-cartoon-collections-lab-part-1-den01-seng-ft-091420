6d072a17d6db278e55dbed9401c7129b5b977843
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-30 18:44:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ end
 
 def list_dwarves(array)
   hash = Hash.new
-  %w(Dopey Grumpy Bashful).each_with_index { |item, index|
+  %w(1. Dopey 2. Grumpy 3. Bashful).each_with_index { |item, index|
     hash[item] = index
   }
   hash
